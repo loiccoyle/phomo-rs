cba578542eaf1842e074676f9e7cba4f82f471ad
Loic Coyle <loic.coyle@hotmail.fr>
2024-10-12 01:32:20 +0200

    feat: add cli crate

---
+++ phomo-cli/src/cli.rs
@@ -0,0 +1,79 @@
+extern crate clap;
+use std::{path::PathBuf, str::FromStr};
+
+use clap::Parser;
+use clap_verbosity_flag::Verbosity;
+
+#[derive(Debug, Clone)]
+pub(crate) struct TwoNumbers(pub u32, pub u32);
+
+impl FromStr for TwoNumbers {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parts: Vec<&str> = s.split(',').collect();
+        if parts.len() != 2 {
+            return Err("Expected two numbers separated by a comma".into());
+        }
+
+        let first = parts[0]
+            .trim()
+            .parse::<u32>()
+            .map_err(|_| "Invalid number for first value")?;
+        let second = parts[1]
+            .trim()
+            .parse::<u32>()
+            .map_err(|_| "Invalid number for second value")?;
+        Ok(TwoNumbers(first, second))
+    }
+}
+
+#[derive(Parser, Debug)]
+#[command(version, author = "Loic Coyle")]
+#[clap(author = "Loic Coyle")]
+#[command(group(
+    clap::ArgGroup::new("transfer")
+        .required(false)  // Set to `true` if you want one flag to be mandatory
+        .args(&["transfer_tiles_to_master", "transfer_master_to_tiles"]),
+))]
+#[command(group(
+    clap::ArgGroup::new("tile_resize")
+        .required(false)  // Set to `true` if you want one flag to be mandatory
+        .args(&["crop_tiles", "resize_tiles"]),
+))]
+pub struct Arguments {
+    /// Master image.
+    #[arg( value_hint=clap::ValueHint::FilePath)]
+    pub master_file: PathBuf,
+    /// Tile directory.
+    #[arg(value_hint=clap::ValueHint::DirPath)]
+    pub tile_dir: PathBuf,
+    /// Output mosaic file.
+    #[arg(value_hint=clap::ValueHint::FilePath)]
+    pub output: PathBuf,
+
+    /// Grid size, the number of tiles along the width and height.
+    ///
+    /// If not provided, the grid size will be set to a sane value depending on the number of tiles images.
+    #[arg(short = 'g', long, value_name = "WIDTH,HEIGHT")]
+    pub grid_size: Option<TwoNumbers>,
+    /// Crop tiles to grid cell size.
+    #[arg(long)]
+    pub crop_tiles: bool,
+    /// Resize tiles to grid cell size.
+    #[arg(long)]
+    pub resize_tiles: bool,
+
+    /// Equalize the master and tile image color distributions.
+    #[arg(long)]
+    pub equalize: bool,
+    /// Transfer the color palette of the master image to the tile images.
+    #[arg(long)]
+    pub transfer_master_to_tiles: bool,
+    /// Transfer the color palette of the tile images to the master image.
+    #[arg(long)]
+    pub transfer_tiles_to_master: bool,
+
+    #[command(flatten)]
+    pub verbose: Verbosity,
+}
---
+++ phomo-cli/src/main.rs
@@ -0,0 +1,119 @@
+use std::error::Error;
+
+extern crate env_logger;
+use clap::Parser;
+use log::info;
+use phomo::{
+    read_images_from_dir, read_images_from_dir_cropped, read_images_from_dir_resized, ColorMatch,
+    Mosaic,
+};
+
+mod cli;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // Parse the CLI arguments
+    let args = cli::Arguments::parse();
+    env_logger::Builder::new()
+        .filter_level(args.verbose.log_level_filter())
+        .init();
+
+    let mut master_img = image::open(args.master_file)
+        .map_err(|e| format!("Failed to read master image: {}", e))?
+        .to_rgb8();
+    info!(
+        "Master image size: {}x{}",
+        master_img.width(),
+        master_img.height()
+    );
+
+    let tile_count = args
+        .tile_dir
+        .read_dir()?
+        .filter_map(|entry| {
+            let entry = entry.ok()?; // Skip invalid entries
+            let path = entry.path();
+            if path.is_file() {
+                // Try to get the image dimensions, returns None if it's not an image
+                image::image_dimensions(&path).ok()
+            } else {
+                None
+            }
+        })
+        .count();
+    info!("Tile count: {}", tile_count);
+
+    // determine a resonable grid size
+    let (grid_width, grid_height) = match args.grid_size {
+        Some(cli::TwoNumbers(width, height)) => (width, height),
+        None => {
+            // we leave a few tiles to be unused so get better assignments
+            let grid_dim = (tile_count as f32 * 0.8).sqrt().round() as u32;
+            (grid_dim, grid_dim)
+        }
+    };
+    info!("Grid size: {}x{}", grid_width, grid_height);
+
+    let (cell_width, cell_height) = (
+        master_img.width() / grid_width,
+        master_img.height() / grid_height,
+    );
+    info!("Cell size: {}x{}", cell_width, cell_height);
+
+    // Read tile images from the directory
+    let mut tile_imgs = if args.crop_tiles {
+        read_images_from_dir_cropped(&args.tile_dir, cell_width, cell_height)
+    } else if args.resize_tiles {
+        read_images_from_dir_resized(
+            &args.tile_dir,
+            cell_width,
+            cell_height,
+            image::imageops::FilterType::Nearest,
+        )
+    } else {
+        read_images_from_dir(&args.tile_dir)
+    }
+    .map_err(|e| format!("Failed to crop tile images: {}", e))?;
+
+    // Handle the different transformations
+    if args.equalize {
+        master_img = master_img.equalize();
+        tile_imgs = tile_imgs.equalize();
+    }
+
+    if args.transfer_master_to_tiles {
+        tile_imgs = tile_imgs.match_palette(&master_img);
+    } else if args.transfer_tiles_to_master {
+        master_img = master_img.match_palette(&tile_imgs);
+    }
+
+    if !tile_imgs
+        .iter()
+        .all(|tile| tile.dimensions() == (cell_width, cell_height))
+    {
+        return Err(format!(
+            "All tile images must have dimensions equal to the grid cell size: {}x{}, consider using the --crop-tiles or --resize-tiles flags",
+            cell_width, cell_height
+        )
+        .into());
+    };
+
+    // Create the mosaic
+    let mosaic = Mosaic::from_images(master_img, tile_imgs, (grid_width, grid_height))
+        .map_err(|e| format!("Failed to create mosaic: {}", e))?;
+
+    // Compute the distance matrix
+    let d_matrix = mosaic.distance_matrix();
+
+    // Build the mosaic image
+    let mosaic_img = mosaic
+        .build(d_matrix)
+        .map_err(|e| format!("Failed to build mosaic image: {}", e))?;
+
+    // Save the final mosaic image to output
+    mosaic_img
+        .save(&args.output)
+        .map_err(|e| format!("Failed to save mosaic image: {}", e))?;
+
+    info!("Mosaic image created successfully: {:?}", args.output);
+    Ok(())
+}
