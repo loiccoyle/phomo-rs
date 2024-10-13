30c1937e8828223910beea43cb424514a58e4e41
Loic Coyle <loic.coyle@hotmail.fr>
2024-10-13 23:36:35 +0200

    feat(wasm): add resizing control, fix color matching

--- phomo-wasm/src/lib.rs
+++ phomo-wasm/src/lib.rs
@@ -1,6 +1,6 @@
 use base64::{engine::general_purpose, Engine as _};
 use image::RgbImage;
-use phomo::{metrics, ColorMatch, Mosaic as MosaicRs};
+use phomo::{metrics, utils, ColorMatch, Mosaic as MosaicRs};
 use std::io::Cursor;
 use wasm_bindgen::prelude::*;
 
@@ -9,12 +9,18 @@ pub fn init_panic_hook() {
     console_error_panic_hook::set_once();
 }
 
+#[wasm_bindgen]
+pub enum ResizeType {
+    Crop,
+    Resize,
+}
+
 // Wrapper for Mosaic struct
 #[wasm_bindgen]
 pub struct Mosaic {
     master_img: RgbImage,
     tile_imgs: Vec<RgbImage>,
-    mosaic: Option<MosaicRs>, // Mosaic is constructed after palette matching or equalization
+    mosaic: Option<MosaicRs>,
     grid_width: u32,
     grid_height: u32,
 }
@@ -27,6 +33,7 @@ impl Mosaic {
         tile_imgs_data: js_sys::Array,
         grid_width: u32,
         grid_height: u32,
+        tile_resize: Option<ResizeType>,
     ) -> Result<Mosaic, JsValue> {
         // Load master image
         let master_img = image::load_from_memory(master_img_data)
@@ -37,10 +44,26 @@ impl Mosaic {
         let tile_imgs = (0..tile_imgs_data.length())
             .map(|i| {
                 let data = js_sys::Uint8Array::new(&tile_imgs_data.get(i)).to_vec();
-                image::load_from_memory(&data)
+                let img = image::load_from_memory(&data)
                     .map_err(|err| JsValue::from(err.to_string()))
                     .unwrap()
-                    .to_rgb8()
+                    .to_rgb8();
+
+                match tile_resize {
+                    Some(ResizeType::Resize) => image::imageops::resize(
+                        &img,
+                        master_img.width() / grid_width,
+                        master_img.height() / grid_height,
+                        image::imageops::FilterType::Nearest,
+                    ),
+                    Some(ResizeType::Crop) => utils::crop_imm_centered(
+                        &img,
+                        master_img.width() / grid_width,
+                        master_img.height() / grid_height,
+                    )
+                    .to_image(),
+                    None => img,
+                }
             })
             .collect();
 
@@ -54,45 +77,34 @@ impl Mosaic {
     }
 
     // Method to equalize the master and tile images
-    pub fn equalize_images(&mut self) -> Result<(), JsValue> {
+    pub fn equalize(&mut self) {
         self.master_img = self.master_img.equalize();
-        self.tile_imgs = self
-            .tile_imgs
-            .iter_mut()
-            .map(|img| img.equalize())
-            .collect();
-        Ok(())
+        self.tile_imgs = self.tile_imgs.equalize();
     }
 
     // Method to transfer master image palette to tile images
-    pub fn transfer_master_to_tiles(&mut self) -> Result<(), JsValue> {
-        self.tile_imgs = self
-            .tile_imgs
-            .iter_mut()
-            .map(|img| img.match_palette(&self.master_img))
-            .collect();
-        Ok(())
+    #[wasm_bindgen(js_name = transferMasterToTiles)]
+    pub fn transfer_master_to_tiles(&mut self) {
+        self.tile_imgs = self.tile_imgs.match_palette(&self.master_img);
     }
 
     // Method to transfer tile images palette to master image
-    pub fn transfer_tiles_to_master(&mut self) -> Result<(), JsValue> {
+    #[wasm_bindgen(js_name = transferTilesToMaster)]
+    pub fn transfer_tiles_to_master(&mut self) {
         self.master_img = self.master_img.match_palette(&self.tile_imgs);
-        Ok(())
     }
 
     // Build the mosaic after applying palette matching or equalization
-    pub fn build_mosaic(&mut self, metric_type: &str) -> Result<String, JsValue> {
+    pub fn build(&mut self, metric_type: &str) -> Result<String, JsValue> {
         // Ensure the mosaic is constructed only once after transformations
-        if self.mosaic.is_none() {
-            self.mosaic = Some(
-                MosaicRs::from_images(
-                    self.master_img.clone(),
-                    self.tile_imgs.clone(),
-                    (self.grid_width, self.grid_height),
-                )
-                .map_err(|err| JsValue::from(err.to_string()))?,
-            );
-        }
+        self.mosaic = Some(
+            MosaicRs::from_images(
+                self.master_img.clone(),
+                self.tile_imgs.clone(),
+                (self.grid_width, self.grid_height),
+            )
+            .map_err(|err| JsValue::from(err.to_string()))?,
+        );
 
         let mosaic = self.mosaic.as_ref().unwrap();
 
