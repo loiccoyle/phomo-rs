66ccb4fd94c2e26636ca05d4d21a69187cc08771
Loic Coyle <loic.coyle@hotmail.fr>
2024-10-20 01:34:05 +0200

    feat: add grid overlay img generation

--- phomo/src/master.rs
+++ phomo/src/master.rs
@@ -1,7 +1,7 @@
 use std::path::Path;
 
 extern crate image;
-use image::{GenericImageView, RgbImage};
+use image::{GenericImage, GenericImageView, Rgb, RgbImage};
 use log::{debug, info};
 
 use crate::error::Error;
@@ -15,6 +15,7 @@ pub struct Master {
     pub cells: Vec<RgbImage>,
     /// The size of each grid cell, in pixels.
     pub cell_size: (u32, u32),
+    grid_size: (u32, u32),
 }
 
 /// Represents the master image.
@@ -31,11 +32,13 @@ impl Master {
         let (img_width, img_height) = img.dimensions();
         // the number of cells in each dimension of the grid
         let (grid_width, grid_height) = grid_size;
+        let (cell_width, cell_height) = (img_width / grid_width, img_height / grid_height);
+        info!("Grid Cell size: {}x{}", cell_width, cell_height);
         // How much is left over around the grid
         let (width_extra, height_extra) = (img_width % grid_width, img_height % grid_height);
 
         let img = if width_extra > 0 || height_extra > 0 {
-            info!("Cropping image to fit grid size");
+            info!("Cropping image to fit grid");
             let img_cropped =
                 utils::crop_imm_centered(&img, img_width - width_extra, img_height - height_extra);
             img_cropped.to_image()
@@ -44,11 +47,12 @@ impl Master {
         };
         debug!("Image dimensions: {}x{}", img.width(), img.height());
 
-        let (cells, cell_size) = Self::construct_regions(&img, grid_size)?;
+        let cells = Self::construct_regions(&img, grid_size)?;
         Ok(Master {
             img,
             cells,
-            cell_size,
+            cell_size: (cell_width, cell_height),
+            grid_size,
         })
     }
 
@@ -62,21 +66,16 @@ impl Master {
         Self::from_image(img, grid_size)
     }
 
-    fn construct_regions(
-        img: &RgbImage,
-        grid_size: (u32, u32),
-    ) -> Result<(Vec<RgbImage>, (u32, u32)), Error> {
+    fn construct_regions(img: &RgbImage, grid_size: (u32, u32)) -> Result<Vec<RgbImage>, Error> {
         let (grid_width, grid_height) = grid_size;
+        let (cell_width, cell_height) = (img.width() / grid_width, img.height() / grid_height);
         if img.width() % grid_width != 0 || img.height() % grid_height != 0 {
             return Err(
-                format!("Invalid grid shape, the imge dimensions: {}x{} are not divisible by the grid shape: {}x{}", 
-                    img.width(), img.height(), grid_width, grid_height)
+                format!("Invalid grid shape, the imge dimensions: {}x{} are not divisible by the cell size: {}x{}", 
+                    img.width(), img.height(), cell_width, cell_height)
                     .into(),
             );
         }
-
-        let (cell_width, cell_height) = (img.width() / grid_width, img.height() / grid_height);
-        info!("Grid Cell size: {}x{}", cell_width, cell_height);
 
         let cells = (0..grid_height)
             .flat_map(|y| {
@@ -86,7 +85,27 @@ impl Master {
                 })
             })
             .collect();
-        Ok((cells, (cell_width, cell_height)))
+        Ok(cells)
+    }
+
+    /// Overlay the grid onto the master image.
+    pub fn overlay_grid(&self) -> Result<RgbImage, Error> {
+        let (grid_width, grid_height) = self.grid_size;
+        let mut grid_img = RgbImage::from_pixel(
+            self.img.width() + grid_width - 1,
+            self.img.height() + grid_height - 1,
+            Rgb([255, 255, 255]),
+        );
+
+        for (i, region) in self.cells.iter().enumerate() {
+            let col_i = i as u32 % grid_width;
+            let row_i = i as u32 / grid_width;
+            let x = col_i * self.cell_size.0 + col_i;
+            let y = row_i * self.cell_size.1 + row_i;
+            grid_img.copy_from(region, x, y)?;
+        }
+
+        Ok(grid_img)
     }
 }
 
@@ -186,13 +205,30 @@ mod tests {
         let img = create_test_image(256, 256);
         let grid_size = (4, 4);
 
-        let (cells, _) = Master::construct_regions(&img, grid_size).unwrap();
+        let cells = Master::construct_regions(&img, grid_size).unwrap();
         // Assert that the cells have been divided correctly
         assert_eq!(cells.len(), 16); // 4x4 grid
         for region in &cells {
             assert_eq!(region.width(), 64); // Each region should be 64x64
             assert_eq!(region.height(), 64);
         }
+    }
+
+    #[test]
+    fn test_overlay_grid() {
+        let img = create_test_image(128, 256);
+        let grid_size = (4, 4);
+        let master = Master::from_image(img, grid_size).unwrap();
+        let grid_overlay = master.overlay_grid();
+        assert!(grid_overlay.is_ok());
+        let grid_overlay = grid_overlay.unwrap();
+
+        let expected_path = test_dir().join("grid_overlay.png");
+        if std::env::var("PHOMO_UPDATE_EXPECTED").is_ok() {
+            grid_overlay.save(&expected_path).unwrap();
+        }
+        let expected_img = image::open(expected_path).unwrap().to_rgb8();
+        assert_eq!(expected_img, grid_overlay);
     }
 
     #[test]
