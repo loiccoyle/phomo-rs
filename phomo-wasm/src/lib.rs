e28e460d1110d813a5e7c6432fc9c18c96225a0c
Loic Coyle <loic.coyle@hotmail.fr>
2025-01-25 18:57:53 +0100

    fix: cleanup interface & add docs

--- phomo-wasm/src/lib.rs
+++ phomo-wasm/src/lib.rs
@@ -1,7 +1,8 @@
 use base64::{engine::general_purpose, Engine as _};
 use image::RgbImage;
 use phomo::{
-    metrics, utils, Auction, Blueprint, ColorMatch, Greedy, Master as MasterRs, Mosaic as MosaicRs,
+    metrics, utils, Auction, Blueprint, ColorMatch, Greedy, Hungarian, Master as MasterRs,
+    Mosaic as MosaicRs,
 };
 use phomo::{DistanceMatrix, SolverConfig};
 use std::io::Cursor;
@@ -14,12 +15,29 @@ pub fn init_panic_hook() {
     console_error_panic_hook::set_once();
 }
 
+/// Metric types
+#[wasm_bindgen]
+pub enum MetricType {
+    NormL1,
+    NormL2,
+}
+
+/// Tile resizing types
 #[wasm_bindgen]
 pub enum ResizeType {
     Crop,
     Resize,
 }
 
+/// Tile to cell solvers.
+#[wasm_bindgen]
+pub enum Solver {
+    Greedy,
+    Hungarian,
+    Auction,
+}
+
+/// Overlay a grid on the master image
 #[wasm_bindgen(js_name = overlayGrid)]
 pub fn overlay_grid(
     master_img_data: &[u8],
@@ -47,6 +65,16 @@ pub struct Mosaic {
 
 #[wasm_bindgen]
 impl Mosaic {
+    /// Create a new Mosaic instance.
+    ///
+    /// # Arguments
+    /// - `master_img_data`: The master image data as a byte array.
+    /// - `tile_imgs_data`: The tile images data as a JS array of byte arrays.
+    /// - `grid_width`: The number of grid columns.
+    /// - `grid_height`: The number of grid rows.
+    /// - `max_tile_occurrences`: The maximum number of times a tile can be used in the mosaic.
+    /// - `tile_resize`: The type of tile resizing to apply.
+    /// - ''master_resize`: The desired size of the master image after resizing.
     #[wasm_bindgen(constructor)]
     pub fn new(
         master_img_data: &[u8],
@@ -115,19 +143,19 @@ impl Mosaic {
         })
     }
 
-    // Method to equalize the master and tile images
+    /// Equalize the master and tile images
     pub fn equalize(&mut self) {
         self.inner.master.img = self.inner.master.img.equalize();
         self.inner.tiles = self.inner.tiles.equalize();
     }
 
-    // Method to transfer master image palette to tile images
+    /// Transfer master image palette to tile images
     #[wasm_bindgen(js_name = transferMasterToTiles)]
     pub fn transfer_master_to_tiles(&mut self) {
         self.inner.tiles = self.inner.tiles.match_palette(&self.inner.master.img);
     }
 
-    // Method to transfer tile images palette to master image
+    /// Transfer tile images palette to master image
     #[wasm_bindgen(js_name = transferTilesToMaster)]
     pub fn transfer_tiles_to_master(&mut self) -> Result<(), JsValue> {
         self.inner.master = MasterRs::from_image(
@@ -138,18 +166,20 @@ impl Mosaic {
         Ok(())
     }
 
-    fn distance_matrix_with_metric(&self, metric_type: &str) -> Result<DistanceMatrix, JsValue> {
+    fn distance_matrix_with_metric(
+        &self,
+        metric_type: MetricType,
+    ) -> Result<DistanceMatrix, JsValue> {
         let metric = match metric_type {
-            "NormL1" => metrics::norm_l1,
-            "NormL2" => metrics::norm_l2,
-            _ => return Err(JsValue::from("Unsupported metric type")),
+            MetricType::NormL1 => metrics::norm_l1,
+            MetricType::NormL2 => metrics::norm_l2,
         };
 
         Ok(self.inner.distance_matrix_with_metric(metric))
     }
 
-    // Build the mosaic after applying palette matching or equalization
-    pub fn build(&self, metric_type: &str) -> Result<String, JsValue> {
+    /// Build the mosaic img using the Hungarian algorithm and return it as base64 encoded PNG.
+    pub fn build(&self, metric_type: MetricType) -> Result<String, JsValue> {
         let d_matrix = self.distance_matrix_with_metric(metric_type)?;
 
         let mosaic_img = self
@@ -160,30 +190,31 @@ impl Mosaic {
         to_base64(mosaic_img)
     }
 
-    #[wasm_bindgen(js_name = buildGreedy)]
-    pub fn build_greedy(&self, metric_type: &str) -> Result<String, JsValue> {
+    /// Build the mosaic img using the specified solver and return it as base64 encoded PNG.
+    #[wasm_bindgen(js_name = buildWithSolver)]
+    pub fn build_with_solver(
+        &self,
+        metric_type: MetricType,
+        solver: Solver,
+    ) -> Result<String, JsValue> {
         let d_matrix = self.distance_matrix_with_metric(metric_type)?;
-
-        let mosaic_img = self
-            .inner
-            .build_with_solver(d_matrix, Greedy::new(self.solver_config.clone()))
-            .map_err(|err| JsValue::from(err.to_string()))?;
+        let mosaic_img = match solver {
+            Solver::Greedy => self
+                .inner
+                .build_with_solver(d_matrix, Greedy::new(self.solver_config.clone())),
+            Solver::Hungarian => self
+                .inner
+                .build_with_solver(d_matrix, Hungarian::new(self.solver_config.clone())),
+            Solver::Auction => self
+                .inner
+                .build_with_solver(d_matrix, Auction::new(1, self.solver_config.clone())),
+        }
+        .map_err(|err| JsValue::from(err.to_string()))?;
 
         to_base64(mosaic_img)
     }
 
-    #[wasm_bindgen(js_name = buildAuction)]
-    pub fn build_auction(&self, metric_type: &str) -> Result<String, JsValue> {
-        let d_matrix = self.distance_matrix_with_metric(metric_type)?;
-
-        let mosaic_img = self
-            .inner
-            .build_with_solver(d_matrix, Auction::new(1, self.solver_config.clone()))
-            .map_err(|err| JsValue::from(err.to_string()))?;
-
-        to_base64(mosaic_img)
-    }
-
+    /// Get the tile images as base64 encoded PNGs.
     #[wasm_bindgen(js_name = getTiles)]
     pub fn get_tiles(&self) -> Result<Vec<String>, JsValue> {
         let mut tiles = Vec::with_capacity(self.inner.tiles.len());
@@ -194,13 +225,15 @@ impl Mosaic {
         Ok(tiles)
     }
 
+    /// Get the master image as base64 encoded PNG.
     #[wasm_bindgen(js_name = getMaster)]
     pub fn get_master(&self) -> Result<String, JsValue> {
         to_base64(self.inner.master.img.clone())
     }
 
+    /// Build the mosaic blueprint using the Hungarian algorithm and return it as JSON.
     #[wasm_bindgen(js_name = buildBlueprint)]
-    pub fn build_blueprint(&self, metric_type: &str) -> Result<JsValue, JsValue> {
+    pub fn build_blueprint(&self, metric_type: MetricType) -> Result<JsValue, JsValue> {
         let d_matrix = self.distance_matrix_with_metric(metric_type)?;
 
         let mosaic = self
@@ -211,29 +244,31 @@ impl Mosaic {
         Ok(serde_wasm_bindgen::to_value(&mosaic)?)
     }
 
-    #[wasm_bindgen(js_name = buildBlueprintGreedy)]
-    pub fn build_blueprint_greedy(&self, metric_type: &str) -> Result<JsValue, JsValue> {
+    /// Build the mosaic blueprint using the specified solver and return it as JSON.
+    #[wasm_bindgen(js_name = buildBlueprintWithSolver)]
+    pub fn build_blueprint_with_solver(
+        &self,
+        metric_type: MetricType,
+        solver: Solver,
+    ) -> Result<JsValue, JsValue> {
         let d_matrix = self.distance_matrix_with_metric(metric_type)?;
+        let blueprint = match solver {
+            Solver::Greedy => self
+                .inner
+                .build_blueprint_with_solver(d_matrix, Greedy::new(self.solver_config.clone())),
+            Solver::Hungarian => self
+                .inner
+                .build_blueprint_with_solver(d_matrix, Hungarian::new(self.solver_config.clone())),
+            Solver::Auction => self
+                .inner
+                .build_blueprint_with_solver(d_matrix, Auction::new(1, self.solver_config.clone())),
+        }
+        .map_err(|err| JsValue::from(err.to_string()))?;
 
-        let mosaic = self
-            .inner
-            .build_blueprint_with_solver(d_matrix, Greedy::new(self.solver_config.clone()))
-            .map_err(|err| JsValue::from(err.to_string()))?;
-
-        Ok(serde_wasm_bindgen::to_value(&mosaic)?)
-    }
-    #[wasm_bindgen(js_name = buildBlueprintAuction)]
-    pub fn build_blueprint_auction(&self, metric_type: &str) -> Result<JsValue, JsValue> {
-        let d_matrix = self.distance_matrix_with_metric(metric_type)?;
-
-        let mosaic = self
-            .inner
-            .build_blueprint_with_solver(d_matrix, Auction::new(1, self.solver_config.clone()))
-            .map_err(|err| JsValue::from(err.to_string()))?;
-
-        Ok(serde_wasm_bindgen::to_value(&mosaic)?)
+        Ok(serde_wasm_bindgen::to_value(&blueprint)?)
     }
 
+    /// Render the mosaic blueprint and return it as base64 encoded PNG.
     #[wasm_bindgen(js_name = renderBlueprint)]
     pub fn render_blueprint(&self, blueprint: JsValue) -> Result<String, JsValue> {
         let blueprint = serde_wasm_bindgen::from_value::<Blueprint>(blueprint)?;
@@ -245,8 +280,8 @@ impl Mosaic {
     }
 }
 
+/// Convert the mosaic image to base64 encoded PNG.
 fn to_base64(img: RgbImage) -> Result<String, JsValue> {
-    // Convert the mosaic image to base64 encoded PNG
     let mut buf: Vec<u8> = Vec::new();
     img.write_to(&mut Cursor::new(&mut buf), image::ImageFormat::Png)
         .map_err(|err| JsValue::from(err.to_string()))?;
