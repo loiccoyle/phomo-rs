191c87d303cef1a82754f9b400b161565809566b
Jonah <97376233+jonahclarsen@users.noreply.github.com>
2025-02-05 14:48:13 -0400

    optimize norm_l1

--- phomo/src/metrics.rs
+++ phomo/src/metrics.rs
@@ -17,11 +17,15 @@ pub(crate) type MetricFn = fn(&RgbImage, &RgbImage) -> i64;
 /// assert_eq!(norm, 255 * 3 * 2 * 2);
 /// ```
 pub fn norm_l1(img1: &RgbImage, img2: &RgbImage) -> i64 {
-    img1.pixels().zip(img2.pixels()).fold(0, |sum, (p1, p2)| {
-        sum + (p1[0].abs_diff(p2[0]) as i64)
-            + (p1[1].abs_diff(p2[1]) as i64)
-            + (p1[2].abs_diff(p2[2]) as i64)
-    })
+    let data1 = img1.as_raw();
+    let data2 = img2.as_raw();
+    
+    assert_eq!(data1.len(), data2.len(), "Images must have the same number of pixels");
+    
+    data1.iter()
+        .zip(data2.iter())
+        .map(|(byte1, byte2)| i64::from(byte1.abs_diff(*byte2)))
+        .sum()
 }
 
 /// L2 norm, the euclidean distance of the pixels.
